35f64925cee2514b7e47b4444bbc1f4822596952
Chris Visser <2496739+chris-visser@users.noreply.github.com>
2025-05-16 23:37:10 +0200

    feat: Nuxt SSR/SSR support on useConvexQuery

--- playground/convex/tasks.ts
+++ playground/convex/tasks.ts
@@ -1,8 +1,27 @@
-import { query } from './_generated/server'
+import { v } from 'convex/values'
+import { mutation, query } from './_generated/server'
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query('tasks').collect()
+  },
+})
+
+export const add = mutation({
+  args: { text: v.string() },
+  handler: async (ctx, args) => {
+    return ctx.db.insert('tasks', { text: args.text })
+  },
+})
+
+export const remove = mutation({
+  args: { id: v.id('tasks') },
+  handler: async (ctx, args) => {
+    const task = await ctx.db.get(args.id)
+    if (!task) {
+      throw new Error('Task not found')
+    }
+    await ctx.db.delete(task._id)
   },
 })
--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,20 @@
+const allowConvexVueLocalPackage = {
+  server: {
+    fs: {
+      allow: [
+        '../../convex-vue',
+        './',
+      ],
+    },
+  },
+}
+
 export default defineNuxtConfig({
   modules: ['../src/module'],
+  // ssr: false,
   devtools: { enabled: true },
   compatibilityDate: '2025-04-24',
+  vite: allowConvexVueLocalPackage,
   convex: {
     url: process.env.NUXT_CONVEX_URL,
   },
